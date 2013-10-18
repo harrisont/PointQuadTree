db48e26d2eb7ff3005355a4b44ffae7bbf210368
Harrison Ting <runhappy@gmail.com>
2013-10-18 00:23:59 -0700

    Added magnitude methods to Point

--- point.py
+++ point.py
@@ -5,6 +5,24 @@ class Point:
         self.x = x
         self.y = y
 
+    def magnitude(self):
+        """
+        >>> Point(3, 4).magnitude()
+        5.0
+        >>> Point(-5, -12).magnitude()
+        13.0
+        """
+        return math.sqrt(self.magnitude_squared())
+
+    def magnitude_squared(self):
+        """
+        >>> Point(3, 4).magnitude_squared()
+        25
+        >>> Point(-5, -12).magnitude_squared()
+        169
+        """
+        return self.x**2 + self.y**2
+
     def distance(self, other_point):
         """
         >>> p = Point(1, 2)
@@ -12,10 +30,28 @@ class Point:
         1.0
         >>> p.distance(Point(4, 6))
         5.0
+        >>> p.distance(Point(-3, -1))
+        5.0
         >>> p.distance(p)
         0.0
         """
-        return math.sqrt((self.x - other_point.x)**2 + (self.y - other_point.y)**2)
+        return math.sqrt(self.distance_squared(other_point))
+
+    def distance_squared(self, other_point):
+        """
+        >>> p = Point(1, 2)
+        >>> p.distance_squared(Point(2, 2))
+        1
+        >>> p.distance_squared(Point(4, 6))
+        25
+        >>> p.distance_squared(Point(-3, -1))
+        25
+        >>> p.distance_squared(p)
+        0
+        """
+        return (self.x - other_point.x)**2 + (self.y - other_point.y)**2
+
+
 
     def __lt__(self, other):
         """
